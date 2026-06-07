7d7e519169e5cd9b09e8e2e41ac625d4b64454e6
TanakaDaiki-57413 <274158570+TanakaDaiki-57413@users.noreply.github.com>
2026-06-07 13:31:58 +0000

    [update]ホストアクセス許可コマンド追記

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,9 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  #ホストアクセス許可
+  config.hosts.clear
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Make code changes take effect immediately without server restart.
